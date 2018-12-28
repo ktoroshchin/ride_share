a520728d3047950833124a8f9d1f260d84fc75c8
Macbook <k.toroshchin@gmail.com>
2018-12-28 00:31:12 -0500

    changed index.js

---
+++ client/build/precache-manifest.7aa56a4ed34026d1bfbfa82442ca9817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8dc59c8e136d5dfa5c39",
+    "url": "/static/js/main.8dc59c8e.chunk.js"
+  },
+  {
+    "revision": "a4cb189dbd6a40596460",
+    "url": "/static/js/1.a4cb189d.chunk.js"
+  },
+  {
+    "revision": "8dc59c8e136d5dfa5c39",
+    "url": "/static/css/main.2296a869.chunk.css"
+  },
+  {
+    "revision": "a4cb189dbd6a40596460",
+    "url": "/static/css/1.9eb1b1f7.chunk.css"
+  },
+  {
+    "revision": "b5cba7b666c81f3882d81bb33ecbf73c",
+    "url": "/index.html"
+  }
+];
