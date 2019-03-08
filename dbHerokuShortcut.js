5d3eaf593c981af0a71bd25e643144fb3e8bd06b
ktoroshchcin <k.toroshchin@gmail.com>
2019-03-08 16:25:31 -0500

    refactor

--- dbHerokuShortcut.js
+++ dbHerokuShortcut.js
@@ -3,16 +3,16 @@ CREATE TABLE reservations(
     first_name varchar(250),
     leaving_from varchar(250),
     going_to varchar(250),
-    phone_number integer,
+    phone_number varchar(250),
     email varchar(250),
     departure_date varchar(250),
     departure_time varchar(250),
-    number_of_people integer,
+    number_of_people varchar(250)),
     is_confirmed BOOL DEFAULT 'f',
     message varchar(250),
-    driver_name NOT NULL varchar(250),
-    driver_id integer,
-    created_at timestamp not null default CURRENT_TIMESTAMP);
+    driver_name varchar(250),
+    driver_id varchar(250),
+    created_at timestamp without time zone default (now() at time zone 'utc’));
 
 CREATE TABLE drivers (
     id serial primary key,
@@ -21,4 +21,4 @@ CREATE TABLE drivers (
     password varchar(250),
     vehicle_type varchar(250),
     email varchar(250),
-    created_at timestamp not null default CURRENT_TIMESTAMP);
+    created_at timestamp without time zone default (now() at time zone 'utc’));
