d28d836ccaca47c65c2d8c657ee6098968d067ae
ktoroshchcin <k.toroshchin@gmail.com>
2019-03-08 15:15:59 -0500

    Refactor and heroku deploy

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,4 @@
 
-/*jshint esversion: 6 */
 import React, { Component } from "react";
 import {
   Collapse,
@@ -24,14 +23,49 @@ class NavBar extends Component {
       this.setState({
         isOpen: !this.state.isOpen
       });
+    };
+
+  renderAdmin(){
+    if(this.props.getUserName() === null){
+      return (
+        <React.Fragment>
+          <NavItem>
+            <NavLink className="nav-link" to="/login">
+              DRIVER LOGIN
+            </NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink className="nav-link" to="/register">
+              DRIVER REGISTER
+            </NavLink>
+          </NavItem>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <NavItem>
+            <span className="greeting">
+              Hello, {this.props.getUserName().first_name}
+            </span>
+            <NavLink className="nav-link" to="/my-reservations">
+              RESERVATIONS
+            </NavLink>
+          </NavItem>
+          <NavItem onClick={this.props.deleteUser}>
+            <NavLink to="/" className="nav-link logout-link" >
+              LOGOUT
+            </NavLink>
+          </NavItem>
+        </React.Fragment>
+      )
     }
+  }
 
 
 
   render(){
-    var username = this.props.getUserName()
-
-    if(username === null){
     return(
     <div className="top-nav">
       <Navbar className="navbar" dark expand="md">
@@ -39,36 +73,11 @@ class NavBar extends Component {
         <NavbarToggler onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="ml-auto" navbar>
-            <NavItem >
-              <NavLink className="nav-link" to="/login">DRIVER LOGIN</NavLink>
-            </NavItem>
-            <NavItem >
-              <NavLink className="nav-link" to="/register">DRIVER REGISTER</NavLink>
-            </NavItem>
+            {this.renderAdmin()}
           </Nav>
         </Collapse>
       </Navbar>
       </div>
-    )
-  }
-    return(
-      <div className="top-nav">
-        <Navbar dark expand="md">
-          <NavbarBrand className="logo" href="/">RideSsshare</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <span className="greeting">Hello, {username.first_name}</span>
-                  <NavLink className="nav-link" to="/my-reservations">RESERVATIONS</NavLink>
-                </NavItem>
-                <NavItem  onClick={this.props.deleteUser}>
-                  <NavLink className="nav-link logout-link" to="/">LOGOUT</NavLink>
-                </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-     </div>
     )
   }
 }
